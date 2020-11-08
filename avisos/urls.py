b7a2d15e403921fcd8b414359a1b75d9f69eaed2
Seba Valdivia <sebastianvaldivita@gmail.com>
2020-11-08 15:28:14 -0300

    Add create avisos view

--- avisos/urls.py
+++ avisos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import AvisosListView
+from .views import AvisosListView, AvisosCreateView
 
 urlpatterns = [
     path('', AvisosListView.as_view(), name='avisos-list'),
+    path('nuevo/', AvisosCreateView.as_view(), name='nuevo-aviso')
 ]
--- avisos/views.py
+++ avisos/views.py
@@ -1,4 +1,6 @@
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 from .models import Aviso
 # Create your views here.
@@ -7,4 +9,11 @@ class AvisosListView(ListView):
     model = Aviso
     paginate_by = 10
     template_name = 'aviso_list.html'
+    ordering = ['-created_at']
 
+class AvisosCreateView(CreateView):
+
+    model = Aviso
+    fields = '__all__'
+    template_name = 'aviso_form.html'
+    success_url = reverse_lazy('home')
